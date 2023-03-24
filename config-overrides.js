f2ebd43c3e506f9c38c98d5e52eebf7f101a1cdb
Guru Nanma P <42802604+18nanma@users.noreply.github.com>
2023-03-23 20:07:12 -0500

    reversed themis

--- config-overrides.js
+++ config-overrides.js
@@ -1,30 +1,25 @@
-const fs = require('fs');
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CWD_PATH = fs.realpathSync(process.cwd());
-const buildPath = process.env.BUILD_PATH || 'build';
-const resolveApp = relativePath => path.resolve(CWD_PATH, relativePath);
-const WASM_PATH = path.join(CWD_PATH, 'node_modules', 'wasm-themis/src/libthemis.wasm');
-const SCRIPTS_PATH = path.join(resolveApp(buildPath), 'static/js/');
+
 module.exports = function override(config, env) {
-   if (!config.plugins) {
-       config.plugins = [];
-   }
-   config.plugins.push(
-       new CopyWebpackPlugin({ patterns: [{ from: WASM_PATH, to: SCRIPTS_PATH }] })
-   );
-   config['resolve']['fallback'] = {
-       crypto: false,
-       fs: false,
-       http: false,
-       https: false,
-       net: false,
-       path: false,
-       stream: false,
-       tls: false,
-       util: false,
-       url: false,
-       zlib: false,
-   }
-   return config;
-}
+  const wasmExtensionRegExp = /\.wasm$/;
+
+  config.resolve.extensions.push('.wasm');
+
+  config.module.rules.forEach(rule => {
+    (rule.oneOf || []).forEach(oneOf => {
+      if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
+        // make file-loader ignore WASM files
+        oneOf.exclude.push(wasmExtensionRegExp);
+      }
+    });
+  });
+
+  // add a dedicated loader for WASM
+  config.module.rules.push({
+    test: wasmExtensionRegExp,
+    include: path.resolve(__dirname, 'src'),
+    use: [{ loader: require.resolve('wasm-loader'), options: {} }]
+  });
+
+  return config;
+};
--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import './App.css';
 // import Htm from './Test';
-import { initialize } from 'wasm-themis';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    initialize().then(() => {
-        console.log("Themis initialized");
-    }).catch( err => {
-        // Themis initialization must be only once.
-    })
-}, []);
   return (
     <div className="App">
       <header className="App-header">

e14f61a0f4586a94f068f358861852af6ad7369a
Guru Nanma P <42802604+18nanma@users.noreply.github.com>
2023-03-23 20:37:29 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 // import Htm from './Test';
 import React, { useState, useEffect } from 'react';
+import Iframe from 'react-iframe'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-
+      <Iframe url="https://18nanma.github.io/whisper/"
+        width="640px"
+        height="320px"
+        id=""
+        className=""
+        display="block"
+        position="relative"/>
         {/* <Htm/> */}
         Hello
         

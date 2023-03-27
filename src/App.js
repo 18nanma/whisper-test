94fd82edc148b38271672e7f18f60e20a0504754
Guru Nanma P <42802604+18nanma@users.noreply.github.com>
2023-03-27 07:09:39 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
     };
 
     handleClickDownloadTiny();
+    handleClickDownloadTiny();
+    handleClickDownloadTiny();
 
     window.addEventListener('storage', handleStorageChange);
 
